b368eeef2a17a5fb137182f1b429ee17f52df2d7
POdPeShIk <VoLnA_209@mail.ru>
2024-04-24 19:46:40 +0300

    Tpublish photo to Telegram channel

--- tg.py
+++ tg.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 if __name__ == "__main__":
     load_dotenv(".env")
     token=os.environ['TOKEN']
+    chat_id= "@testingmain"
+    file_id='images/APOD/image_apod_1.jpg'
     bot = telegram.Bot(token="7023144175:AAFNrL2XdYfO1olmN8gDxc7py26eInzOw6g")
     bot.send_message(chat_id="@testingmain", text="I'm sorry Dave I'm afraid I can't do that.")
+    message = bot.send_document(chat_id="@testingmain", document=open('images/APOD/image_apod_1.jpg', 'rb'))
+    message.document.file_id
     print(bot.get_me())
